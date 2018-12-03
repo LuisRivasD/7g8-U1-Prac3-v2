143f4132caf41cb07cdcfb21c751c72253e886f8
Luis Rivas <luisrivasd@gmail.com>
2018-12-03 07:59:31 -0600

    Se añadieron los archivos de la practica terminada

---
+++ sitioGraduaciones/index.php
@@ -0,0 +1,96 @@
+<?php
+	include_once("header.html");
+    include_once("footer.html");
+    include_once("sideBar.html");
+    
+?>
+   
+   <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="inicio">
+        <div class="my-auto">
+          <h1 class="mb-0">Graduaciones
+            <span class="text-primary">Buhescas</span>
+          </h1>
+          <div class="subheading mb-5">Oriente 9 s/n Col. Emiliano Zapata CP94320 · +52 (272)724-4096 ·
+            <a href="mailto:name@email.com">buhos@promnights.com</a>
+          </div>
+          <p class="lead mb-5">Bienvenido al sitio de los creadores de las mejores experiencias para tu prom night. Este es nuestro sitio dedicado especialmente para ti, amigo Búho.</p>
+          <div class="iconos">
+            <a href="#">
+              <img id="logITO" src="img/logito.png" alt="">
+            </a>
+            <a href="#">
+              <img id="logITO" src="img/clase2015.jpg" alt="">
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="costos">
+        <div class="my-auto">
+          <h2 class="mb-5">Costos</h2>            
+        </div>
+      </section>
+      <hr class="m-0">
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="publicidad">
+        <div class="my-auto">
+          <h2 class="mb-5">Públicidad</h2>
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Nuestros paquetes lo incluyen T-O-D-O</h3>
+              <div class="subheading mb-3">Comunicación Básica </div>
+              <div>·Enviamos tus invitaciones por redes sociales: Facebook, Whatsapp e Instagram</div>
+              <div>·Le recordamos a tus invitados una semana, tres días, 24 y 12 horas antes del evento</div>
+              <div>·Generamos un grupo donde se podra compartir todo el contenido que ustedes gusten</div>
+              <br><br>
+              <div class="subheading mb-3">Comunicación Intermedia </div>
+              <div>Todas las anteriores mas:</div>
+              <div>·Creación de un trending topic a través de todos los canales de redes sociales para que todo el mundo se entere de tu evento</div>
+              <div>·Posteamos en mas redes sociales: Twitter, Messenger, Reddit, Snapchat</div>
+              <br><br>
+              <div class="subheading mb-3">Comunicación Avanzada </div>
+              <div>Todas las anteriores mas:</div>
+              <div>Grabamos todo un livestream a 3 camaras para que nadie se pierda de la diversión</div>
+              <div>Sitio Web dedicado a tu evento</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr class="m-0">
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="galeria">
+        <div class="my-auto">
+          <h2 class="mb-5">Galería</h2>
+        </div>
+      </section>
+      <hr class="m-0">
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="contacto">
+        <div class="my-auto">
+        <h2 class="mb-5">Contacto</h2>
+        </div>
+      </section>
+      <hr class="m-0">
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="avisoLegal">
+        <div class="my-auto">
+          <h2 class="mb-5">Aviso Legal</h2>
+           <p>ACCESO Y CONDICIONES DE USO DEL SITIO WEB
+            Las presentes Condiciones de Uso (En adelante las “Condiciones”),  tienen por finalidad facilitar al usuario información relativa a FLUIDRA, sus actividades y productos, noticias publicadas, así como sus distintas áreas de negocio.
+
+            Este Sitio Web tiene carácter informativo.
+
+            Tanto la navegación, como el acceso a la información publicada en el Sitio Web, suponen la aceptación como Usuario, sin reservas de ninguna clase, de todas y cada una de las presentes Condiciones. Si no acepta las presentes condiciones, le rogamos se abstenga de utilizar el Sitio Web y su contenido.</p> 
+        </div>
+      </section>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/resume.min.js"></script>
